8ea524f95411a4445a756c7fe67ed5841b35b3f8
RmLtA <rm.liantsoa@gmail.com>
2013-04-17 23:23:00 +0200

    test de la classe OneWordInstruction

---
+++ Malai_master/src/org/malai/sphinx4/interaction/library/test.java
@@ -0,0 +1,21 @@
+package org.malai.sphinx4.interaction.library;
+
+public class test {
+	
+	private static OneWordInstruction o;
+
+	public static void main(String[] args) {
+		setO(new OneWordInstruction());
+		
+	}
+
+	public static OneWordInstruction getO() {
+		return o;
+	}
+
+	public static void setO(OneWordInstruction o) {
+		test.o = o;
+	}
+	
+
+}
