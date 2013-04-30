183ad236aaa73d82879c3bd0b218de5d6671c237
RmLtA <rm.liantsoa@gmail.com>
2013-04-30 14:30:48 +0200

    SpeechEventHandler

--- Malai_master/src/org/malai/sphinx4/interaction/SpeechEventHandler.java
+++ Malai_master/src/org/malai/sphinx4/interaction/SpeechEventHandler.java
@@ -1,12 +1,12 @@
 package org.malai.sphinx4.interaction;
 
-import edu.cmu.sphinx.recognizer.Recognizer;
+/*import edu.cmu.sphinx.recognizer.Recognizer;*/
 
 
 public interface SpeechEventHandler{
 	
   void onSpeech(String word);
 
-  void onSpeechStateChanged(Recognizer.State status);
+  /*void onSpeechStateChanged(Recognizer.State status);*/
   
 }
